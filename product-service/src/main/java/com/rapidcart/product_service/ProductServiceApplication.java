3ae25a66c85790b275f0b45802fefb77f267d794
Praneeth Shetty <praneethshetty626@gmail.com>
2025-10-30 19:41:23 +0530

    updated implementation for initial setup

---
+++ product-service/src/main/java/com/rapidcart/product_service/ProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rapidcart.product_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceApplication.class, args);
+	}
+
+}
---
+++ product-service/src/test/java/com/rapidcart/product_service/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rapidcart.product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
